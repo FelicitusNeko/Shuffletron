9080320516112ef77ab4248748ee21c86c54ba02
Kewlio <kewlio-origin@simguy.net>
2021-09-03 17:44:21 -0300

    frontend chore: add Shuffletron interfaces

---
+++ src/tsx/interfaces/Shuffletron.ts
@@ -0,0 +1,14 @@
+export interface STList {
+  id: number;
+  name: string;
+}
+
+export interface STGame {
+  id: number;
+  listId: number;
+  name: string;
+  displayName: string;
+  description: string;
+  weight: number;
+  status: number;
+}
