c98ae6ead09d7502d26ebcdd0df563b27160ff06
Felix R <felicitusneko@duck.com>
2024-05-14 15:06:36 -0300

    update route rendering for Router 6

--- src/tsx/App.tsx
+++ src/tsx/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 
 import Chat from './routes/Chat';
 import STDisplay from './routes/STDisplay';
@@ -8,18 +8,18 @@ import STEntry from './routes/STEntry';
 const App: React.FC = () => {
   return (
     <div className='App'>
-      <Switch>
-        <Route path='/chatoverlay' component={Chat} />
-        <Route path='/st-entry' component={STEntry} />
-        <Route path='/st-display' component={STDisplay} />
-        <Route>
+      <Routes>
+        <Route path='/chatoverlay' element={<Chat />} />
+        <Route path='/st-entry' element={<STEntry />} />
+        <Route path='/st-display' element={<STDisplay />} />
+        <Route path='*' element={
           <ul>
             <li><Link to='/chatoverlay'>Chat overlay</Link></li>
             <li><Link to='/st-entry'>Shuffletron Entry</Link></li>
             <li><Link to='/st-display'>Shuffletron Display</Link></li>
           </ul>
-        </Route>
-      </Switch>
+        } />
+      </Routes>
     </div>
   );
 }
